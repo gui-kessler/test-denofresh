88c082b8fd18209470ff381e959982dc2b5e9616
Guilherme Kessler <guilhermekessler@outlook.com>
2023-05-18 22:37:28 -0300

    init

---
+++ routes/about.tsx
@@ -0,0 +1,20 @@
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+    async GET(req: any, ctx: any) {
+        const resp = await ctx.render();
+        resp.headers.set("X-Custom-Header", "Hello");
+        return resp;
+    }
+}
+
+export default function About () {
+    return (
+        <>
+            <main>
+                <h1>About</h1>
+                <p>This is the about page</p>
+            </main>
+        </>
+    );
+}
